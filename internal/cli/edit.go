2a5943af0c52ee7a654174b1b5526bd8ba08ddd2
Eduardo Silva <eduardo4silva@gmail.com>
2026-07-15 18:31:34 +0200

    refactor(cli): move edit subcommand into internal/cli package

---
+++ internal/cli/edit.go
@@ -0,0 +1,20 @@
+package edit
+
+import (
+	"os"
+	"os/exec"
+)
+
+func EditFile(path string) error {
+	editor := os.Getenv("EDITOR")
+	if editor == "" {
+		editor = "vi"
+	}
+
+	cmd := exec.Command(editor, path)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	return cmd.Run()
+}
